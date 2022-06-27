82f3aab80b193b203503c4b34739896a136e153d
InkaOro <65492226+InkaOro@users.noreply.github.com>
2022-06-27 22:58:50 +0100

    updates

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,3 @@
 class ArtistsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :artists ]
+  skip_before_action :authenticate_user!, only: [ :home, :index ]
 end
